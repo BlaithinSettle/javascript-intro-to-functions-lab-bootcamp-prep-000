9047656538dc09a9a9a4a4473508ec5ccd957f35
Blaithin Settle <blaithin.settle@yahoo.com>
2018-08-23 19:04:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string===string.toLowerCase()){
-        return 'I can\'t hear you'
+        return "I can\'t hear you!"
     } else if (string===string.toUpperCase()){
-      return 'YES INDEED!'
+      return "YES INDEED!"
     } else{
-      return 'I love you, Grandma.'
+      return "I love you, too."
     }
 }
 
