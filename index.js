063f949c43d0c15896112154d712c45cc594bfde
Blaithin Settle <blaithin.settle@yahoo.com>
2018-08-23 18:47:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string){
+return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  var upperCase= string.toUpperCase()
+  console.log(upperCase)
+
+}
+function logWhisper(string){
+  var lowerCase=string.toLowerCase()
+  console.log(lowerCase)
+}
+function logWhisper(string){
+    var lowerCase=string.toLowerCase()
+    console.log(lowerCase)
+}
+/*describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })*/
+
+
+
+
